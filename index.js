998483182fc443837e25324ca5865f5461d296f8
tiancristian <clcristianlacroix@gmail.com>
2024-04-10 00:04:41 -0300

    Added functionality > Search for Cryptos

--- index.js
+++ index.js
@@ -13,13 +13,22 @@ app.get("/", async (req, res) => {
     const result = await axios.get(API_URL + "tickers/BTC-USD");
 
     res.render("index.ejs", { content: result.data });
-    console.log(result.data.symbol);
-    // console.log(req.body.data.symbol);
+    console.log(result.data);
+  } catch (error) {
+    res.render("index.ejs", { content: JSON.stringify(error.response) });
+  }
+});
+
+app.post("/get-ticker", async (req, res) => {
+  try {
+    const result = await axios.get(API_URL + "tickers/" + req.body.ticker);
+    const form = req.body.ticker;
+    res.render("index.ejs", { content: result.data });
+    console.log(form);
+    console.log(result.data);
   } catch (error) {
     res.render("index.ejs", { content: JSON.stringify(error.response.data) });
   }
-
-  //   res.render("index.ejs", { content: "Waiting for data..." });
 });
 
 app.listen(port, () => {
--- notes.md
+++ notes.md
@@ -11,3 +11,19 @@
 3. Include EJS for templating.
 
 4. Ensure that the project has a structured directory and file organization.
+
+---
+
+5. Choose an API to interact with
+
+   > Blockchain.com
+
+6. Fetch basic data to show in the homepage
+
+7. Add functionalities :
+   Search for an specific currency : ex: BTC-USD, ETH-USD
+
+- Ludic turn: A crystal ball to predict the price of a selected currency in the future
+
+8. Page style :
+   CSS , colors , buttons, divisions and layout
