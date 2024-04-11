6e6ebca1712d0fa73363535ea69e3d7ee3c97c24
tiancristian <clcristianlacroix@gmail.com>
2024-04-10 22:07:09 -0300

    Bootstrap implementation

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 const API_URL = "https://api.blockchain.com/v3/exchange/";
+app.use(express.static("/public/"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
